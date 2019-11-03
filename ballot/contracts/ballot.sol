458d287e450e4515342717c8986b3ec3a4cea21d
Takashi Saito <takashi11171117@gmail.com>
2019-11-03 23:54:08 +0900

    Added struce

--- ballot/contracts/ballot.sol
+++ ballot/contracts/ballot.sol
@@ -1,9 +1,25 @@
 pragma solidity >=0.5.11;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+
+    modifier restricted() {
+        require(
+            msg.sender == manager,
+            "required manager"
+        );
+        _;
+    }
 
     constructor(uint minimum) public {
         manager = msg.sender;
